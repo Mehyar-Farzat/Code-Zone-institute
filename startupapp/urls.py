366b2ab4caf50e98d56a893c8120830dd4bfba47
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-07 18:00:58 +0200

    create about, contatct pages

--- startupapp/urls.py
+++ startupapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from startupapp import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+
+
+
     
 ]
--- startupapp/views.py
+++ startupapp/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
